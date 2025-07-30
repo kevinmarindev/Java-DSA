93f454e09280c5f6337b124be5f5c9d1d0dfae75
kevinmarindev <kevin.marin7@hotmail.com>
2025-07-29 23:14:01 -0500

    solved  binary search in java

---
+++ src/SearchAlgos/BinarySearch2.java
@@ -0,0 +1,22 @@
+package SearchAlgos;
+
+public class BinarySearch2 {
+
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5,6,7,8};
+        System.out.println(search(arr, 3, 0, arr.length - 1));
+    }
+    public static int search(int[] arr, int target, int start, int end){
+        if(arr.length == 0  || start > end || end >= arr.length) return -1;
+
+        int mid = (start + end) / 2;
+        if(arr[mid] == target){
+            return mid;
+        }
+        if(arr[mid] > target){
+             return search(arr, target, start, mid - 1);
+        }
+
+        return search(arr, target, mid + 1, end);
+    }
+}
